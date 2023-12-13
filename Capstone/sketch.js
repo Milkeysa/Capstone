a46f1a2d9b7b0f7e892a50275839b9b628b8e1ac
DerinsolaB <145162184+DerinsolaB@users.noreply.github.com>
2023-12-13 15:20:30 -0600

    we glitched on accident
    day 3

--- Capstone/sketch.js
+++ Capstone/sketch.js
@@ -1,17 +1,68 @@
+
 // Project Title
-// Your Name
+// Derinsola Bolaji & Milkeysa Mohamed Jamal
 // Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-
-//hi this is milkey
+// Global Variables
+let player;
+let opponent = [];
+let playerX = 200;
+let playerY = 200;
+let playerView;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  //calling the functions
+  playerView = map(mouseX, 0, windowWidth, playerX, playerY);
+  
+}
+
+function keyPressed(){
 }
 
 function draw() {
   background(220);
+  //drawing the functions
+  playerMoves();
 }
+
+//starting 2d okie dokie
+// player creation
+// movement creation and control
+function playerMoves(){
+  player = square(playerX,playerY,20);
+  //restricting player to canvas
+  if(key === "s"|| key === "S"){
+    playerY = playerY + 10;
+  }
+  if(key === "a"|| key === "A"){
+    playerX = playerX - 10;
+  }
+  if(key === "d"|| key === "D"){
+    playerX = playerX + 10;
+  }
+  if(key=== "w"|| key === "W"){
+    playerY = playerY - 10;
+  }
+  if(playerX-10 === 0){
+    playerX = playerX + 10;
+  }
+  if(playerX+10 === width){
+    playerX = playerX - 10;
+  }
+  if(playerY-10 === 0){
+    playerY = playerY + 10;
+  }
+  if(playerY === height){
+    playerY = playerY - 10;
+  }
+}
+// mapping function
+
+// other players/opponents
+
+// gun creation (SAFE AND PG)   ps we dont know how to be mean yet
+
+//this part is for rng(damage,health,drop,spawns)
+
