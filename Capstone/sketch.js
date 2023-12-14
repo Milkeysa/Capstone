de6f20f6c57bc74e4efe3690fbcb829af7637320
DerinsolaB <145162184+DerinsolaB@users.noreply.github.com>
2023-12-14 15:21:50 -0600

    Day 4

--- Capstone/sketch.js
+++ Capstone/sketch.js
@@ -1,68 +1,79 @@
-
-// Project Title
+// DOOM game - Capstone Project
 // Derinsola Bolaji & Milkeysa Mohamed Jamal
 // Date
 
 // Global Variables
-let player;
+let player; let playerView;
 let opponent = [];
-let playerX = 200;
-let playerY = 200;
-let playerView;
+let playerX = 200; let playerY = 200;
+
 
 function setup() {
-  createCanvas(400, 400);
-  rectMode(CENTER);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  box()
   //calling the functions
   playerView = map(mouseX, 0, windowWidth, playerX, playerY);
-  
-}
-
-function keyPressed(){
 }
 
 function draw() {
   background(220);
   //drawing the functions
+  player = square(playerX,playerY,20);
+  rectMode(CENTER);
   playerMoves();
 }
 
-//starting 2d okie dokie
+//starting 2d 
 // player creation
+
+function keyPressed(){
 // movement creation and control
+  
+}
+
 function playerMoves(){
-  player = square(playerX,playerY,20);
+  let border = 10;
   //restricting player to canvas
-  if(key === "s"|| key === "S"){
-    playerY = playerY + 10;
+  if(key === "s"|| key === "S" || keyCode === DOWN_ARROW ){
+    playerY = playerY + border;
   }
-  if(key === "a"|| key === "A"){
-    playerX = playerX - 10;
+  if(key === "a"|| key === "A" || keyCode === LEFT_ARROW){
+    playerX = playerX - border;
   }
-  if(key === "d"|| key === "D"){
-    playerX = playerX + 10;
+  if(key === "d"|| key === "D"|| keyCode === RIGHT_ARROW){
+    playerX = playerX + border;
   }
-  if(key=== "w"|| key === "W"){
-    playerY = playerY - 10;
+  if(key=== "w"|| key === "W" || keyCode === UP_ARROW){
+    playerY = playerY - border;
   }
-  if(playerX-10 === 0){
-    playerX = playerX + 10;
+  if(playerX-border === windowWidth){
+    playerX = playerX + border;
   }
-  if(playerX+10 === width){
-    playerX = playerX - 10;
+  if(playerX+border === windowWidth){
+    playerX = playerX - border;
   }
-  if(playerY-10 === 0){
-    playerY = playerY + 10;
+  if(playerY-border === windowHeight){
+    playerY = playerY + border;
   }
-  if(playerY === height){
-    playerY = playerY - 10;
+  if(playerY+border === windowHeight){
+    playerY = playerY - border;
   }
 }
+
 // mapping function
+//should start 3d not 2d
+function basicMap(){
+  translate(0,200);
+  box(0,250,100);
+  
+}
+function startingMap(){
 
+}
 // other players/opponents
+//can start 2d
 
-// gun creation (SAFE AND PG)   ps we dont know how to be mean yet
+// gun creation (SAFE AND PG) ps we dont know how to be mean yet
 
 //this part is for rng(damage,health,drop,spawns)
 
