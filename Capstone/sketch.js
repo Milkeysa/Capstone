79232a9b2c2691f16b3a45dbf51f20cb0179dc2b
DerinsolaB <145162184+DerinsolaB@users.noreply.github.com>
2023-12-18 15:17:01 -0600

    Day 6

--- Capstone/sketch.js
+++ Capstone/sketch.js
@@ -3,16 +3,16 @@
 // Date
 
 // Global Variables
-let player; 
+let player;
 let playerView;
 let opponent = [];
+let border = 10;
 let playerX = 0;//player starting x point 
 let playerY = 0;//player starting y point
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  
   //calling the functions
   playerView = map(mouseX, 0, windowWidth, playerX, playerY);
   angleMode(DEGREES);
@@ -24,75 +24,77 @@ function draw() {
   fill("red");
   rotateX(80);
   //rotateY(0);
-  box(10000,10000,29);
+  box(10000, 10000, 29);
   rectMode(CENTER);
   playerMoves();
+  strokeWeight(2);
+  noStroke();
   startingMap();
 }
 
 //starting 2d 
 // player creation
 
-function keyPressed(){
-// movement creation and control
+function keyPressed() {
+  // movement creation and control
   
-}
-
-function playerMoves(){
-  let border = 10;
-  push();
-  translate(playerX,playerY);
-  fill("white");
-  box(5,30,60);
-  pop();
-  //restricting player to canvas
-  if(fill ==="red"){
-    playerY = 0;
-    playerX = 0;
-  }
-  if(key === "s"|| key === "S" || keyCode === DOWN_ARROW ){
-    playerY = playerY + border;
-  }
-  if(key === "a"|| key === "A" || keyCode === LEFT_ARROW){
-    playerX = playerX - border;
-  }
-  if(key === "d"|| key === "D"|| keyCode === RIGHT_ARROW){
-    playerX = playerX + border;
-  }
-  if(key=== "w"|| key === "W" || keyCode === UP_ARROW){
-    playerY = playerY - border;
-  }
-  if(playerX-border === -140){
-    playerX = playerX + border;
-  }
-  if(playerX+border === 140){
-    playerX = playerX - border;
-  }
-  if(playerY-border === -140){
-    playerY = playerY + border;
-  }
-  if(playerY+border === 140){
-    playerY = playerY - border;
-  }
 }
 
 // mapping function
 //should start 3d not 2d
-function basicMap(){
-  translate(0,200);
-  box(100,100,200);
+
+function basicMap() {
+  translate(0, 200);
+  box(100, 100, 200);
 }
 
-function startingMap(){
+function startingMap() {
   fill("grey");
   let wallheight = 120;
-  translate(-150,0);
-  box(10,300,wallheight);
-  translate(300,0);
-  box(10,300,wallheight);
-  translate(-150,-150);
-  box(300,10,wallheight);
+  translate(-150, 0);
+  box(10, 300, wallheight);
+  translate(300, 0);
+  box(10, 300, wallheight);
+  translate(-150, -150);
+  box(300, 10, wallheight);
+}
 
+
+function playerMoves() {
+  push();
+  translate(playerX, playerY);
+  fill("white");
+  box(5, 10, 80);
+  pop();
+  //restricting player to canvas
+  if (fill === "red") {
+    playerY = 0;
+    playerX = 0;
+  }
+  if (key === "s" || key === "S" || keyCode === DOWN_ARROW|| onkeydown === "s") {
+    playerY = playerY + border;
+  }
+  if (key === "a" || key === "A" || keyCode === LEFT_ARROW) {
+    playerX = playerX - border;
+  }
+  if (key === "d" || key === "D" || keyCode === RIGHT_ARROW) {
+    playerX = playerX + border;
+  }
+  if (key === "w" || key === "W" || keyCode === UP_ARROW) {
+    playerY = playerY - border;
+  }
+  if (playerX - border === -140) {
+    playerX = playerX + border;
+  }
+  if (playerX + border === 140) {
+    playerX = playerX - border;
+  }
+  if (playerY - border === -140) {
+    playerY = playerY + border;
+  }
+  if (playerY + border === 140) {
+    playerY = playerY - border;
+  }
 }
 // other players/opponents
 //can start 2d*
