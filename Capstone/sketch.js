b12a6f7d3cfe15a54b7a9ea255e5d441d39d124f
DerinsolaB <145162184+DerinsolaB@users.noreply.github.com>
2023-12-19 15:21:56 -0600

    Day 7

--- Capstone/sketch.js
+++ Capstone/sketch.js
@@ -6,13 +6,23 @@
 let player;
 let playerView;
 let opponent = [];
-let border = 10;
+let border = 11;
 let playerX = 0;//player starting x point 
 let playerY = 0;//player starting y point
-
+let easyCam, state = {
+    distance: 208,
+    center  : [5, 3, -13],
+    rotation: [0.3, -0.4, -0.1, 0.85]
+  }, 
+  f, x=0, y=20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  easycam = createEasyCam();
+
+  // set initial camera state
+  //easycam.setState(state, 1000); // animate to state in 1 second
+  //easycam.state_reset = state;   // state to use on reset
   //calling the functions
   playerView = map(mouseX, 0, windowWidth, playerX, playerY);
   angleMode(DEGREES);
@@ -22,14 +32,23 @@ function draw() {
   background("lightblue");
   //drawing the functions
   fill("red");
-  rotateX(80);
   //rotateY(0);
+  // easycam.beginHUD();
+  // noLights();
+  // let state = easycam.getState();
+  // translate(0,playerY/2, 0);
+  
+  translate(playerX*-1, 0,playerY*-1);
+  // rotateZ(playerY/13);
+  rotateX(80);
   box(10000, 10000, 29);
   rectMode(CENTER);
-  playerMoves();
-  strokeWeight(2);
-  noStroke();
   startingMap();
+  playerMoves();
+  
+  strokeWeight(2);
+  //noStroke();
+  
 }
 
 //starting 2d 
@@ -39,7 +58,6 @@ function keyPressed() {
   // movement creation and control
   
 }
-
 // mapping function
 //should start 3d not 2d
 
@@ -57,6 +75,8 @@ function startingMap() {
   box(10, 300, wallheight);
   translate(-150, -150);
   box(300, 10, wallheight);
+
+  
 }
 
 
@@ -71,16 +91,18 @@ function playerMoves() {
     playerY = 0;
     playerX = 0;
   }
-  if (key === "s" || key === "S" || keyCode === DOWN_ARROW|| onkeydown === "s") {
+  if (keyIsDown("s")|| keyIsDown("S") || keyCode === DOWN_ARROW|| onkeydown === "s") {
     playerY = playerY + border;
   }
-  if (key === "a" || key === "A" || keyCode === LEFT_ARROW) {
+  if (keyIsDown("a")|| keyIsDown( "A") || keyCode === LEFT_ARROW) {
     playerX = playerX - border;
+    rotateZ(playerX);
   }
-  if (key === "d" || key === "D" || keyCode === RIGHT_ARROW) {
+  if (keyIsDown("d") || keyIsDown("D") || keyCode === RIGHT_ARROW) {
     playerX = playerX + border;
+    rotateZ(playerX);
   }
-  if (key === "w" || key === "W" || keyCode === UP_ARROW) {
+  if (keyIsDown("w") || keyIsDown("W") || keyCode === UP_ARROW) {
     playerY = playerY - border;
   }
   if (playerX - border === -140) {
